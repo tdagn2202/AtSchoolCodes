568856e6385fcb9b44ec4eb3e39b2747e803c569
tdagn2202 <tranhaidang2222004@gmail.com>
2024-10-16 21:48:00 +0700

    Initial Commit

---
+++ Components/Pages/CalculatorLogic.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+
+namespace revision01.Components.Pages
+{
+    public class CalculatorLogic : ComponentBase
+    {
+        public string num1;
+        public string num2;
+        public string result;
+
+
+
+        public void Add()
+        {
+            result = (Convert.ToDouble(num1) + Convert.ToDouble(num2)).ToString();
+        }
+
+        public void Sub()
+        {
+            result = (Convert.ToDouble(num1) - Convert.ToDouble(num2)).ToString();
+        }
+
+        public void Div()
+        {
+            if (Convert.ToDouble(num2) != 0)
+            {
+                result = (Convert.ToDouble(num1) / Convert.ToDouble(num2)).ToString();
+            }
+            else
+            {
+                result = "Cannot divide by zero!";
+            }
+        }
+
+        public void Mul()
+        {
+            result = (Convert.ToDouble(num1) * Convert.ToDouble(num2)).ToString();
+        }
+
+        public void clear()
+        {
+            num1 = "";
+            num2 = "";
+            result = "";
+        }
+    }
+}
